75d14551245e549e02efe6c2d61a0739b9440782
Imène TOUDEFT <imene.toudeft@hotmail.com>
2024-06-19 10:01:45 +0200

    added router

---
+++ web-service/src/routes.ts
@@ -0,0 +1,26 @@
+import express, { json } from "express";
+const router = express.Router()
+import { getFirstCandidats, getFirstOffres } from "./database";
+
+router.get("/v1/offres", async function (_, res) {
+    try {
+      const offres = await getFirstOffres();
+      res.send(offres);
+    } catch (error) {
+      res.status(500).send({ error: "Internal Server Error", reason: error });
+    }
+  });
+
+router.post("/v1/candidats", async function (req, res) {
+try {
+    const { email } = req.body
+    // recuperer le candidat depuis ma base de donne qui correspond au mail dans le body
+    const candidat = await getFirstCandidats(email)
+    console.log(candidat)
+    res.send(candidat)
+} catch (error) {
+    res.status(500).send({ error: "Internal Server Error", reason: error });
+}
+});
+
+module.exports = router
--- web-service/src/server.ts
+++ web-service/src/server.ts
@@ -1,14 +1,10 @@
 import express, { json } from "express";
 import { auth } from "express-oauth2-jwt-bearer";
 import cors from "cors";
-import { getFirstCandidats, getFirstOffres } from "./database";
 
 const port = 3000;
 const app = express();
-
-// make sure we hare handling CORS properly
-// See more on CORS: https://developer.mozilla.org/en-US/docs/Web/HTTP/CORS
-app.use(cors());
+const router = require('./routes')
 
 const jwtCheck = auth({
   audience: "api.rouge.aus.floless.fr",
@@ -16,31 +12,12 @@ const jwtCheck = auth({
   tokenSigningAlg: "RS256",
 });
 
+app.use(cors());
 // enforce that all incoming requests are authenticated
 app.use(jwtCheck);
 // parse body to json
 app.use(json())
-
-app.get("/v1/offres", async function (_, res) {
-  try {
-    const offres = await getFirstOffres();
-    res.send(offres);
-  } catch (error) {
-    res.status(500).send({ error: "Internal Server Error", reason: error });
-  }
-});
-
-app.post("/v1/candidats", async function (req, res) {
-  try {
-    const { email } = req.body
-    // recuperer le candidat depuis ma base de donne qui correspond au mail dans le body
-    const candidat = await getFirstCandidats(email)
-    console.log(candidat)
-    res.send(candidat)
-  } catch (error) {
-    res.status(500).send({ error: "Internal Server Error", reason: error });
-  }
-});
+app.use('/', router)
 
 app.listen(port, () => {
   console.log(`Server started on http://localhost:${port}`);
