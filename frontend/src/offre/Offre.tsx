6431ef0491782d8e4004115fda8129cc464c03ee
Lucas YALMAN <lucas.yalman@mpublicite.fr>
2024-06-19 11:42:48 +0200

    update page offre

--- frontend/src/offre/Offre.tsx
+++ frontend/src/offre/Offre.tsx
@@ -1,8 +1,9 @@
 import { useAuth0 } from "@auth0/auth0-react";
 import { Box, Button, TextField } from "@mui/material";
-import React, { useEffect } from "react";
+import React from "react";
 import { authenticatedGet } from "../auth/helper";
 import "./offre.scss";
+import { DashboardBox } from "../components/DashboardBox";
 
 export function Offre() {
   const { getAccessTokenSilently } = useAuth0();
@@ -26,7 +27,7 @@ export function Offre() {
       }
     }
     callApi();
-  }, []);
+  }, [getAccessTokenSilently]);
 
   const handleFavoriteToggle = (id: number) => {
     setFavorites((prevFavorites) =>
@@ -59,37 +60,19 @@ export function Offre() {
     console.log("la requête a abouti");
     return (
       <Box>
-        <TextField
-          label="Rechercher"
-          variant="outlined"
-          fullWidth
-          margin="normal"
-          value={search}
-          onChange={handleSearchChange}
-        />
-        <ol>
-          {filteredData.map((offre, index) => (
-            <li key={index}>
-              <h3>{offre.titre_emploi}</h3>
-              <p>Entreprise: {offre.entreprise}</p>
-              <p>Lieu: {offre.lieu}</p>
-              <p>
-                Contrat: {offre.contrat} ({offre.type_contrat})
-              </p>
-              <p>Description courte: {offre.description_courte}</p>
-              <p>Description: {offre.description}</p>
-              <Button
-                variant="contained"
-                color={favorites.includes(offre.id) ? "secondary" : "primary"}
-                onClick={() => handleFavoriteToggle(offre.id)}
-              >
-                {favorites.includes(offre.id)
-                  ? "Retirer des favoris"
-                  : "Ajouter aux favoris"}
-              </Button>
-            </li>
+        <div className="search_container">
+          <TextField
+            label="Rechercher"
+            value={search}
+            onChange={handleSearchChange}
+            className="search_input"
+          />
+        </div>
+        <ul className="dashboard_box_container">
+          {filteredData?.map((offre: any) => (
+            <DashboardBox offre={offre} key={offre.id} />
           ))}
-        </ol>
+        </ul>
       </Box>
     );
   }
